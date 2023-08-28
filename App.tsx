97064c35a5e84c4097d6523afb9bb5bffd096361
Joabe Oliveira <oliveira_joabe@hotmail.com>
2023-08-28 20:53:12 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.image}>
+        <Text style={styles.text}>26 enterprise</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  },
+});
