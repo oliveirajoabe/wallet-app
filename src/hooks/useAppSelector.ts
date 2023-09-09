2710ff2cff8d34c714aa8129877efe92172a46df
Joabe Oliveira <oliveira_joabe@hotmail.com>
2023-09-09 17:33:09 -0300

    merge

--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -1,22 +1,5 @@
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
-<<<<<<< HEAD
-interface RootReducerState {
-  testes: {
-    testeLoading: boolean;
-    testeData: null;
-  };
-  loginReducer: {
-    login: {
-      data: null;
-      loading: false;
-      error: null;
-    };
-  };
-}
-
-=======
->>>>>>> b12903b30d33706ff1f877eabf2106b359012df8
 // Extensão do useSelector tipado
 export const useAppSelector: TypedUseSelectorHook<RootReducerState> =
   useSelector;
